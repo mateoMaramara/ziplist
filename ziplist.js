700cd150da28683ac2c65f8899e4bd6e668eee22
Mateo_maramara <165224810+mateoMaramara@users.noreply.github.com>
2025-02-10 15:46:32 -1000

    E27 complete - 24 minutes

---
+++ ziplist.js
@@ -0,0 +1,17 @@
+"use strict";
+// function ziplist(list: unknown[], list2: unknown[]): unknown[] {
+//     let result = [];
+//     for(let i = 0; i < list.length; i++){
+//         result.push(list[i], list2[i]);
+//     }
+//     return result;
+// }
+function zipListTheFunctionalWay(list, list2) {
+    const result = [];
+    list.forEach((element, index) => {
+        result.push(element, list2[index]);
+    });
+    return result;
+}
+//console.log(ziplist([1, 2, 3], ['a', 'b', 'c']));
+console.log(zipListTheFunctionalWay([1, 2, 3], ['a', 'b', 'c']));
